e7135d5bde97cb3313a436ac4c89d207f6818469
Svale Fossåskaret <svale@3by5.no>
2019-08-29 13:15:09 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="cover">
+    <video
+      autoplay="autoplay"
+      preload="auto"
+      playsinline
+      muted="muted"
+      loop="loop"
+      poster
+      class="video"
+    >
+      <source :src="videoURL" type="video/mp4" />
+    </video>
+    <Menu />
+  </div>
+</template>
+
+<script>
+import Menu from '~/components/Menu.vue'
+export default {
+  components: {
+    Menu
+  },
+  data() {
+    return {
+      videos: [
+        'Axis-111-_3_Splurge-Qoma-Aleph-2_444.mp4',
+        'Axis-111 _2_Splurge-Qoma-Aleph-2_444.mp4',
+        'Qoma-Aleph_W2_onsdag_sett6.mp4'
+      ]
+    }
+  },
+  computed: {
+    videoURL() {
+      const i = Math.floor(Math.random() * 3)
+      return `video/${this.videos[i]}`
+    }
+  }
+}
+</script>
+
+<style>
+.video {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%) translateY(-50%);
+  min-width: 100%;
+  min-height: 100%;
+  max-width: none;
+  max-height: none;
+  width: auto;
+  height: auto;
+  z-index: -1;
+  overflow: hidden;
+}
+</style>

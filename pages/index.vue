0f09cfd3ebc8fd3a08a1f9c5f4607a01be9bdb3f
Svale Fossåskaret <svale@3by5.no>
2019-09-06 14:56:41 +0200

    Resonsive Mobil friendly frontpage And clickable videos

--- pages/index.vue
+++ pages/index.vue
@@ -25,22 +25,48 @@
     <transition name="slideup">
       <div
         v-if="isDone"
-        class="min-h-101 flex justify-between items-center container mx-auto"
+        class="min-h-101 flex flex-col md:flex-row justify-center md:justify-between items-center container mx-auto"
       >
-        <article class="text-center w-1/3 px-4 md:px-8 lg:px-16">
-          <h1 class="mb-4">I</h1>
-          <video-component file-name="rawmaterial_480.mp4" :loop="true" />
-          <n-link class="link mt-4 block" to="/material">Raw Material</n-link>
+        <article
+          class="flex items-center md:block text-center w-4/6 md:w-1/3 md:px-4 md:px-8 lg:px-16 mb-10 mt-10 md:mb-0 md:mt-0"
+        >
+          <h1 class="md:mb-4 w-1/4 md:w-auto">I</h1>
+          <n-link class="link md:mt-4 block w-2/4 md:w-auto" to="/material">
+            <video-component file-name="rawmaterial_480.mp4" :loop="true" />
+          </n-link>
+          <n-link
+            class="link md:mt-4 block w-1/4 flex-grow md:w-auto"
+            to="/material"
+            >Raw Material</n-link
+          >
         </article>
-        <article class="text-center w-1/3 px-4 md:px-8 lg:px-16">
-          <h1 class="mb-4">II</h1>
-          <video-component file-name="process_480.mp4" :loop="true" />
-          <n-link class="mt-4 block" to="/process">Process</n-link>
+
+        <article
+          class="flex items-center md:block text-center w-4/6 md:w-1/3 md:px-4 md:px-8 lg:px-16 mb-10 md:mb-0"
+        >
+          <h1 class="md:mb-4 w-1/4 md:w-auto">II</h1>
+          <n-link class="link md:mt-4 block w-2/4 md:w-auto" to="/process">
+            <video-component file-name="process_480.mp4" :loop="true" />
+          </n-link>
+          <n-link
+            class="link md:mt-4 block w-1/4 flex-grow md:w-auto"
+            to="/process"
+            >Process</n-link
+          >
         </article>
-        <article class="text-center w-1/3 px-4 md:px-8 lg:px-16">
-          <h1 class="mb-4">III</h1>
-          <video-component file-name="outcome_480.mp4" :loop="true" />
-          <n-link class="mt-4 block" to="/outcome">Outcome</n-link>
+
+        <article
+          class="flex items-center md:block text-center w-4/6 md:w-1/3 md:px-4 md:px-8 lg:px-16 mb-0"
+        >
+          <h1 class="md:mb-4 w-1/4 md:w-auto">III</h1>
+          <n-link class="link md:mt-4 block w-2/4 md:w-auto" to="/outcome">
+            <video-component file-name="outcome_480.mp4" :loop="true" />
+          </n-link>
+          <n-link
+            class="link md:mt-4 block w-1/4 flex-grow md:w-auto"
+            to="/outcome"
+            >Outcome</n-link
+          >
         </article>
       </div>
     </transition>

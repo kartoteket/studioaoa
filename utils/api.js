40a718279391b5b56f52019bc114857ca5885553
Svale Fossåskaret <svale@3by5.no>
2020-02-03 14:27:07 +0100

    Add sanity client to build dynamiv outcome routes

--- utils/api.js
+++ utils/api.js
@@ -1,21 +1,33 @@
 import axios from 'axios'
 import Papa from 'papaparse'
+import sanityClient from '@sanity/client'
 import { getPrev, getNext } from '../utils/index'
+
+const client = sanityClient({
+  projectId: 'a98bcom9', // string, required
+  dataset: 'production', // string, required
+  token: '', // or leave blank to be anonymous user
+  useCdn: true // `false` if you want to ensure fresh data
+})
+const query = '*[_type == "work"]{slug}'
+
 const URL =
   'https://docs.google.com/spreadsheets/d/e/2PACX-1vTs9xaw1tFZI0cSEfZXJehljJz20P2G2NUca6KxOqo9u2i0cd0oJCJF_eaZrCtZZ2oVBm22NxnWJf_L/pub?output=csv'
 
-// Staticly generate routes for outcome pages
-const outcomeRoutes = Array.from(Array(16)).map((e, i) => `/outcome/${i}`)
-
 // Dynamicly generate routes for material pages
 export default async function dynamicRoutes() {
-  // console.log('Hitting the API whie generating routes')
-  const res = await axios.get(URL)
-  const { data } = Papa.parse(res.data, {
+  // currently a bot messy with 2 endpoints
+  const [result1, result2] = await Promise.all([
+    client.fetch(query),
+    axios.get(URL)
+  ])
+
+  const { data } = Papa.parse(result2.data, {
     header: true,
     dynamicTyping: true
   })
 
+  const outcomeRoutes = result1.map((d) => `/outcome/${d.slug.current}`)
   const materialRoutes = data.map((c, i, a) => {
     const payload = {
       prev: getPrev(a, c.id),
@@ -27,5 +39,6 @@ export default async function dynamicRoutes() {
       payload
     }
   })
+
   return materialRoutes.concat(outcomeRoutes)
 }

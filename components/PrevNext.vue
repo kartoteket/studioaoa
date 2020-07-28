969a04155f9b5c6561f245fbe3cc523eaa018cf1
Svale Fossåskaret <svale@3by5.no>
2020-07-28 10:26:56 +0200

    Fix page layout (flex) issue

--- components/PrevNext.vue
+++ components/PrevNext.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <nuxt-link v-if="url" :to="url" class="prevnext">
+  <div class="prevnext w-1/5 inline-flex items-center p-4 my-32 opacity-25">
+    <nuxt-link v-if="url" :to="url">
       <svg
         class="fill-current text-ocher inline-block h-12 w-12"
         viewBox="0 0 11 28"
@@ -26,7 +26,6 @@ export default {
 <style>
 .prevnext {
   transition: opacity 0.3s ease;
-  @apply w-1/5 inline-flex items-center p-4 my-32 opacity-25;
 }
 .prevnext:hover {
   @apply opacity-100;

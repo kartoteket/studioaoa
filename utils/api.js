2726512b05cd70a8b01334ade472f5c05f6080fb
Svale Fossåskaret <svale@3by5.no>
2020-07-28 12:56:12 +0200

    Clean up generated dynamic routes

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,4 @@
-import axios from 'axios'
-import Papa from 'papaparse'
 import sanityClient from '@sanity/client'
-import { getPrev, getNext } from '../utils/index'
 
 const client = sanityClient({
   projectId: 'a98bcom9', // string, required
@@ -9,36 +6,32 @@ const client = sanityClient({
   token: '', // or leave blank to be anonymous user
   useCdn: true // `false` if you want to ensure fresh data
 })
-const query = '*[_type == "work"]{slug}'
 
-const URL =
-  'https://docs.google.com/spreadsheets/d/e/2PACX-1vTs9xaw1tFZI0cSEfZXJehljJz20P2G2NUca6KxOqo9u2i0cd0oJCJF_eaZrCtZZ2oVBm22NxnWJf_L/pub?output=csv'
+const query = `{
+  "work": *[_type == "work"]{
+    _id, title, slug, year, body,
+    "embed": asset.embedUrl,
+    "image": asset.image.asset->url,
+    "video": asset.video.asset->url,
+    "categories": categories[]->title
+  },
+  "axis": *[_type == "axis"]{_id, id, title, slug, text, "imageUrl": axisTot.asset->url}
+}`
 
-// Dynamicly generate routes for material pages
+// Dynamicly generate routes
 export default async function dynamicRoutes() {
-  // currently a bit messy with 2 endpoints
-  const [result1, result2] = await Promise.all([
-    client.fetch(query),
-    axios.get(URL)
-  ])
+  const { work, axis } = await client.fetch(query)
 
-  const { data } = Papa.parse(result2.data, {
-    header: true,
-    dynamicTyping: true
-  })
+  const outcomeRoutes = work.map((d) => ({
+    route: `/outcome/${d.slug.current}`,
+    payload: d
+  }))
 
-  const outcomeRoutes = result1.map((d) => `/outcome/${d.slug.current}`)
-  const materialRoutes = data.map((c, i, a) => {
-    const payload = {
-      prev: getPrev(a, c.id),
-      current: c,
-      next: getNext(a, c.id)
-    }
-    return {
-      route: `/material/${c.id}`,
-      payload
-    }
-  })
+  const materialRoutes = axis.map((d) => ({
+    route: `/material/${d.id}`,
+    payload: d
+  }))
 
-  return materialRoutes.concat(outcomeRoutes)
+  const routes = materialRoutes.concat(outcomeRoutes)
+  return routes
 }

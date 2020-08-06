3c3a25789f33423ac9529d510bf251e368f070a0
Svale Fossåskaret <svale@3by5.no>
2020-08-07 00:45:27 +0200

    Add rtf block parser Inkluding youtube videos

---
+++ components/YouTube.vue
@@ -0,0 +1,26 @@
+<template>
+  <youtube
+    :video-id="videoId"
+    :resize="true"
+    :fitParent="true"
+    :width="560"
+    :height="315"
+  ></youtube>
+</template>
+<script>
+import getId from 'get-youtube-id'
+
+export default {
+  props: {
+    url: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      videoId: getId(this.url)
+    }
+  }
+}
+</script>
--- pages/outcome/_id.vue
+++ pages/outcome/_id.vue
@@ -2,14 +2,19 @@
   <div>
     <div class="flex justify-between min-h-screen pt-24">
       <!-- Prev Page -->
-      <prev-next :url="prev" title="Previous page" aria-label="Previous page">
+      <prev-next
+        :url="prev"
+        class="fixed"
+        title="Previous page"
+        aria-label="Previous page"
+      >
         <path
           d="M9.797 8.5c0 0.125-0.063 0.266-0.156 0.359l-6.141 6.141 6.141 6.141c0.094 0.094 0.156 0.234 0.156 0.359s-0.063 0.266-0.156 0.359l-0.781 0.781c-0.094 0.094-0.234 0.156-0.359 0.156s-0.266-0.063-0.359-0.156l-7.281-7.281c-0.094-0.094-0.156-0.234-0.156-0.359s0.063-0.266 0.156-0.359l7.281-7.281c0.094-0.094 0.234-0.156 0.359-0.156s0.266 0.063 0.359 0.156l0.781 0.781c0.094 0.094 0.156 0.219 0.156 0.359z"
         ></path>
       </prev-next>
 
       <article
-        class="h-screen flex flex-col flex-grow justify-around items-center"
+        class="min-h-screen flex flex-col flex-grow justify-around items-center"
       >
         <iframe
           v-if="work.embed"
@@ -39,8 +44,9 @@
           :alt="`Illustration: ${work.title}`"
           class="block max-h-60 max-w-60 mb-8 mx-auto"
         />
+
         <div
-          class="text-center md:text-left md:w-3/5 xxl:w-2/5 px-8 sm:px-0 pb-32"
+          class="text-center md:text-left w-full w-7/8 sm:w-2/4 xxl:w-2/5 px-8 sm:px-0 pb-32"
         >
           <h1 class="heading-1">{{ work.title }} {{ work.year }}</h1>
           <!-- eslint-disable-next-line vue/no-v-html -->
@@ -48,14 +54,23 @@
             v-if="work.body"
             :render-container-on-single-child="true"
             :blocks="work.body"
-            class-name="rtf"
+            class-name="rtf flex flex-col items-center"
+          />
+
+          <!-- eslint-disable-next-line vue/no-v-html -->
+          <block-content
+            v-if="work.content"
+            :render-container-on-single-child="true"
+            :blocks="work.content"
+            :serializers="serializers"
+            class-name="rtf flex flex-col items-center"
           />
         </div>
       </article>
 
       <prev-next
         :url="next"
-        class="justify-end"
+        class="justify-end fixed right-0"
         title="Next page"
         aria-label="Next page"
       >
@@ -68,7 +83,7 @@
     <!-- "Close" -->
     <nuxt-link
       to="/outcome"
-      class="absolute bottom-0 right-0 p-4 m-6 text-3xl font-sans"
+      class="fixed bottom-0 right-0 p-4 m-6 text-3xl font-sans"
       title="Close page"
       aria-label="Close page"
     >
@@ -79,6 +94,8 @@
 
 <script>
 import PrevNext from '@/components/PrevNext'
+import Youtube from '@/components/YouTube'
+import ImageTag from '@/components/ImageTag'
 
 export default {
   components: {
@@ -87,21 +104,28 @@ export default {
   data() {
     return {
       loop: true,
-      id: 1
+      id: 1,
+      serializers: {
+        types: {
+          embedUrl: Youtube,
+          image: ImageTag
+        }
+      }
     }
   },
   async asyncData({ $sanity, params }) {
     const query = `{
       "work": *[_type == "work" && slug.current == "${params.id}"][0]{
-        _id, title, slug, year, body,
+        _id, title, slug, year, body, content[]{..., "imageUrl": asset->url },
         "embed": asset.embedUrl,
         "image": asset.image.asset->url,
         "video": asset.video.asset->url,
         "categories": categories[]->title
       },
-      "entries": *[_type == "work"]{slug}
+      "entries": *[_type == "work" && defined(slug)]{slug}
       }`
     const { work, entries } = await $sanity.fetch(query)
+    console.log(work.content)
     let prev = null
     let next = null
     // out pagination

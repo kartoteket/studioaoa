acf2d6e41d036b3055e3c6e0b309582a0fe565ff
Svale Fossåskaret <svale@3by5.no>
2020-08-09 23:33:25 +0200

    Update hok index

--- pages/hok/index.vue
+++ pages/hok/index.vue
@@ -1,12 +1,16 @@
 <template>
-  <article class="min-h-screen flex flex-col justify-center items-center pt-24">
-    <div class="px-8 text-center sm:px-0 sm:w-2/3 md:w-1/2 lg:w-2/5">
+  <article class="min-h-screen flex flex-col justify-center items-center">
+    <div class="px-8 text-center sm:px-0 w-2/3 md:w-1/2 lg:w-2/5">
       <h1 class="heading-1">
         Molecular Ballet at HOK 23rd of August 2020
       </h1>
+      <p class="mb-12">
+        How can we enlarge our language to describe<br />
+        the territory we have already entered?
+      </p>
       <n-link to="/hok/dancers">
         <video
-          class="max-h-70"
+          class="max-h-50 mx-auto"
           autoplay="autoplay"
           preload="auto"
           playsinline
@@ -14,11 +18,14 @@
           poster
           loop
         >
-          <source src="~assets/videos/process_loop.mp4" type="video/mp4" />
+          <source
+            src="~assets/videos/Cheth 13_1_utviklet 2_2_1_1.mp4"
+            type="video/mp4"
+          />
         </video>
       </n-link>
-      <n-link class="block heading-1 text-2xl vibrate" to="/hok/dancers">
-        Join the Dance!
+      <n-link class="block heading-1 mt-8 vibrate" to="/hok/dancers">
+        Click to join the dance!
       </n-link>
     </div>
   </article>
@@ -27,60 +34,10 @@
 <script>
 export default {
   name: 'HOK',
+  layout: 'hok',
   mounted() {
     localStorage.clear()
   }
 }
 </script>
-<style>
-.vibrate {
-  animation: 2.5s shake cubic-bezier(0.36, 0.07, 0.19, 0.97) infinite alternate;
-  /* animation: shake 0.82s  both; */
-  transform: translate3d(0, 0, 0);
-  backface-visibility: hidden;
-  perspective: 1000px;
-}
-
-/* @keyframes shake {
-  0% {
-    transform: skewX(-15deg);
-  }
-  5% {
-    transform: skewX(15deg);
-  }
-  10% {
-    transform: skewX(-15deg);
-  }
-  15% {
-    transform: skewX(15deg);
-  }
-  20% {
-    transform: skewX(0deg);
-  }
-  100% {
-    transform: skewX(0deg);
-  }
-} */
-@keyframes shake {
-  2%,
-  18% {
-    transform: translate3d(-1px, -1px, 0);
-  }
-
-  4%,
-  16% {
-    transform: translate3d(2px, 1px, 0);
-  }
-
-  6%,
-  10%,
-  14% {
-    transform: translate3d(-4px, -2px, 0);
-  }
-
-  8%,
-  12% {
-    transform: translate3d(4px, 2px, 0);
-  }
-}
-</style>
+<style></style>

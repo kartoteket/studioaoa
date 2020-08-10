16e75dec8a5ff62351e7eae9011f9961713d9c02
Svale Fossåskaret <svale@3by5.no>
2020-08-10 22:41:16 +0200

    Add talking line (re-position dancer / text)

--- pages/hok/guide.vue
+++ pages/hok/guide.vue
@@ -7,112 +7,103 @@
         <img
           :src="`${dancerImage}?w=256`"
           :alt="`#${dancer.title}`"
-          class="block max-h-50 sm:max-h-70"
+          class="block max-h-50 sm:max-h-60 sm:pt-20"
         />
-        <div
-          v-if="step === 1"
-          class="sm:absolute top-0 right-0 text-center mr-10 sm:mr-0"
-        >
-          <h2 class="mb-4">
-            Thank you!<br />
-            My name is {{ dancer.title }}.
-          </h2>
-          <p class="mb-4">
-            <label class="block mb-2">
-              What is your name?
-            </label>
-            <input
-              id="name"
-              @input="updateName($event.target.value)"
-              :value="userName"
-              name="userName"
-              type="text"
-              class="border-black border-dashed  border-b-2 text-center font-sans focus:outline-none mb-4"
-              autofocus
-              required
-            />
-          </p>
-          <p v-if="userName" class="heading-1 vibrate">
-            <button @click="step = 3">> NEXT ></button>
-          </p>
-        </div>
-        <div
-          v-if="step === 2"
-          class="sm:absolute rtf top-0 right-0 text-center mr-10 sm:mr-0"
-        >
-          <p>
-            Our language seems<br />
-            broken!
-          </p>
-          <p>
-            We’ve collected 256<br />
-            symbols to try to<br />
-            make a new one.
-          </p>
-          <p>We call them TOTS</p>
-          <p class="heading-1 vibrate">
-            <button @click="step = 3">> NEXT ></button>
-          </p>
-        </div>
-        <div
-          v-if="step === 3"
-          class="sm:absolute rtf top-0 right-0 text-center mr-10 sm:mr-0"
-        >
-          <p>
-            Which TOT would<br />
-            you like to see danced<br />
-            into life, <u>{{ userName }}</u
-            >?
-          </p>
-          <p>
-            <n-link class="heading-1 vibrate" to="/hok/tots">
-              > NEXT >
-            </n-link>
-          </p>
-        </div>
-        <div
-          v-if="step === 4"
-          class="sm:absolute rtf top-0 right-0 text-center mr-10 sm:mr-0"
-        >
-          <p>
-            Thank you! I will bring your<br />
-            Tot to Høvikodden where it<br />
-            may be picked out to be<br />
-            danced into reality. <br />
-          </p>
-          <p>
-            Please provide a mail address<br />
-            to register for the live event<br />
-            and to review your final<br />
-            choices.<br />
-          </p>
-          <div>
-            <form
-              @submit.prevent="handleSubmit"
-              name="submissions"
-              method="post"
-              data-netlify="true"
-              data-netlify-honeypot="bot-field"
-            >
-              <p class="mb-8">
-                <input type="hidden" name="form-name" value="submissions" />
-                <input :value="userName" type="hidden" name="name" />
-                <input :value="selectedTot" type="hidden" name="tot" />
-                <input
-                  id="email"
-                  @input="updateEmail($event.target.value)"
-                  :value="userEmail"
-                  name="email"
-                  type="email"
-                  class="border-black border-dashed border-b-2 text-center font-sans focus:outline-none"
-                  required
-                  autofocus
-                />
-              </p>
-              <button type="submit" class="heading-1 vibrate">
-                > SUBMIT >
-              </button>
-            </form>
+        <div class="relative rtf text-center mr-10 sm:mr-0 sm:ml-24">
+          <div class="hidden sm:block drawline" />
+          <div v-if="step === 1">
+            <h2 class="mb-4">
+              Thank you!<br />
+              My name is {{ dancer.title }}.
+            </h2>
+            <p class="mb-4">
+              <label class="block mb-2">
+                What is your name?
+              </label>
+              <input
+                id="name"
+                @input="updateName($event.target.value)"
+                :value="userName"
+                name="userName"
+                type="text"
+                class="border-black border-dashed  border-b-2 text-center font-sans focus:outline-none mb-4"
+                autofocus
+                required
+              />
+            </p>
+            <p v-if="userName" class="heading-1 vibrate">
+              <button @click="step = 3">> NEXT ></button>
+            </p>
+          </div>
+          <div v-else-if="step === 2">
+            <p>
+              Our language seems<br />
+              broken!
+            </p>
+            <p>
+              We’ve collected 256<br />
+              symbols to try to<br />
+              make a new one.
+            </p>
+            <p>We call them TOTS</p>
+            <p class="heading-1 vibrate">
+              <button @click="step = 3">> NEXT ></button>
+            </p>
+          </div>
+          <div v-else-if="step === 3">
+            <p>
+              Which TOT would<br />
+              you like to see danced<br />
+              into life, <u>{{ userName }}</u
+              >?
+            </p>
+            <p>
+              <n-link class="heading-1 vibrate" to="/hok/tots">
+                > NEXT >
+              </n-link>
+            </p>
+          </div>
+          <div v-else-if="step === 4">
+            <p>
+              Thank you! I will bring your<br />
+              Tot to Høvikodden where it<br />
+              may be picked out to be<br />
+              danced into reality. <br />
+            </p>
+            <p>
+              Please provide a mail address<br />
+              to register for the live event<br />
+              and to review your final<br />
+              choices.<br />
+            </p>
+            <div>
+              <form
+                @submit.prevent="handleSubmit"
+                name="submissions"
+                method="post"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+              >
+                <p class="mb-8">
+                  <input type="hidden" name="form-name" value="submissions" />
+                  <input :value="userName" type="hidden" name="name" />
+                  <input :value="selectedTot" type="hidden" name="tot" />
+                  <input
+                    id="email"
+                    @input="updateEmail($event.target.value)"
+                    :value="userEmail"
+                    name="email"
+                    type="email"
+                    class="border-black border-dashed border-b-2 text-center font-sans focus:outline-none"
+                    required
+                    autofocus
+                  />
+                </p>
+                <button type="submit" class="heading-1 vibrate">
+                  > SUBMIT >
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
@@ -200,3 +191,14 @@ export default {
   }
 }
 </script>
+<style>
+.drawline {
+  position: absolute;
+  width: 100px;
+  height: 1px;
+  background-color: #000;
+  transform: rotate(-40deg);
+  top: 65px;
+  left: -100px;
+}
+</style>
